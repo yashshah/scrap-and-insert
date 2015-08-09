76a32bda3098516d41a92e5b0586c1956f7b3f0f
Yash Shah <mail@yashshah.com>
2015-08-09 08:11:30 -0500

    Implement mapping and settings through Elastic Search

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import urllib
 import time
 import re
 import os
-import elasticsearch
+from elasticsearch import Elasticsearch
 import eatiht.v2 as v2
 
 appbase_app = "1"
@@ -11,8 +11,19 @@ appbase_app_username = "qHKbcf4M6"
 appbase_app_password = "78a6cf0e-90dd-4e86-8243-33b459b5c7c5"
 
 ##Run the scriping wget script here or another file which calls wget and then this file
-# es = elasticsearch.Elasticsearch()
-es = elasticsearch.Elasticsearch('https://' + appbase_app_username + ':' appbase_app_password + '@scalr.api.appbase.io')
+# es = Elasticsearch()
+# url = "@localhost:9200"
+body_settings = '{ "analysis": { "filter": { "nGram_filter": { "type": "nGram", "min_gram": 2, "max_gram": 20, "token_chars": [ "letter", "digit", "punctuation", "symbol" ] } }, "analyzer": { "nGram_analyzer": { "type": "custom", "tokenizer": "standard", "filter": [ "lowercase", "asciifolding", "nGram_filter" ] }, "body_analyzer": { "type": "custom", "tokenizer": "standard", "filter": [ "lowercase", "asciifolding", "stop", "snowball", "word_delimiter" ] }, "whitespace_analyzer": { "type": "custom", "tokenizer": "standard", "filter": [ "lowercase", "asciifolding" ] } } } }'
+
+body_mapping = '{ "article": { "properties": { "title": { "type": "string", "index_analyzer": "nGram_analyzer", "search_analyzer": "whitespace_analyzer" }, "link": { "type": "string", "index": "not_analyzed" }, "body": { "type": "string", "analyzer": "body_analyzer" } } } }'
+url = '@scalr.api.appbase.io'
+es = Elasticsearch('https://' + appbase_app_username + ':' + appbase_app_password + url)
+es.indices.close(index = appbase_app)
+es.indices.put_settings(index = appbase_app, body = body_settings)
+es.indices.open(index = appbase_app)
+
+print es.indices.put_mapping(index = appbase_app, body = body_mapping, doc_type = appbase_doc_type)
+
 # Make sure that I'm calling to the Appbase elastic search using HTTP Authentication
 # Creating the Mapping with the elastic search
 
